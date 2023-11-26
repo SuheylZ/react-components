de564e5e96e278bab22f2af83e9a72d7abed4436
suheyl z <suheylz@hotmail.com>
2023-11-26 14:02:20 +0500

    Sb errors fixed

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,28 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.stories.tsx"
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
   framework: {
     name: "@storybook/react-vite",
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
-};
-export default config;
+    autodocs: "tag"
+  }
+}
+export default config
+
+
+
+
+
--- src/components/persons/person.stories.tsx
+++ src/components/persons/person.stories.tsx
@@ -1,16 +1,17 @@
 import { Meta, StoryObj } from "@storybook/react"
 import Person from "."
 
-export const meta: Meta<typeof Person> = {
+const meta: Meta<typeof Person> = {
   title: "Components/Person",
   component: Person
 }
+export default meta
 
 
 type Story = StoryObj<typeof Person>
 
 export const story: Story = {
+  name: "Basic",
   render: () => <Person title="Samuel" description="No description" />
 }
 
-export default meta
