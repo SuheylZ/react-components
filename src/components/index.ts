42440f79bb6ce9a33b68f094743bb06641af2b64
suheyl z <suheylz@hotmail.com>
2023-11-26 10:37:06 +0500

    storybookadded

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+export type { LayoutProps } from "./wizard"
+export type { BookProps } from "./wizard"
+export type { PageProps } from "./wizard"
+
+export { Book } from "./wizard"
+export { Page } from "./wizard"
+export { Layout } from "./wizard"
+
+
+
+
+
+
