08b3c410d3fd64f0d8bbc1b58117bd0dd3647ca9
suheyl z <suheylz@hotmail.com>
2023-11-27 21:14:40 +0500

    minor hange in the components index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,12 +2,13 @@ export type { LayoutProps } from "./book"
 export type { BookProps } from "./book"
 export type { PageProps } from "./book"
 export type { StepsProps } from "./book"
-export type { IPageHandler } from "./book"
-export type { PersonProps } from "./persons"
+export type { IPageEvents } from "./book"
+export type { PersonData } from "./persons"
 
 export { Book } from "./book"
 export { Page } from "./book"
 export { Steps } from "./book"
 export { Layout } from "./book"
 export { Person } from "./persons"
+
 
